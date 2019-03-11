657b0c66671179b06f69f863e20007ae87b7d345
mail@marcelpetrick.it <mail@marcelpetrick.it>
2019-03-12 00:14:20 +0100

    humidity: reverted to the state before the adjustment for BME280

--- humidity/basicHumidity_DHT/basicHumidity_DHT.ino
+++ humidity/basicHumidity_DHT/basicHumidity_DHT.ino
@@ -1,14 +1,5 @@
 #include "DHT.h"
 
-// for BME: followed mostly https://randomnerdtutorials.com/esp32-web-server-with-bme280-mini-weather-station/
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
-
-#define SEALEVELPRESSURE_HPA (1013.25)
-Adafruit_BME280 bme; // I2C
-// end of for BME
-
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -30,38 +21,18 @@ DHT dht(DHTPin, DHTTYPE);
 void setup()
 {
   Serial.begin(115200);
-  //Serial.begin(9600);
-  
   Serial.println("DHT22 sensor!");
   //call begin to start sensor
   dht.begin();
-
-  // for the BME
-  Serial.println(F("BME280 test"));
-
-  bool status;
-
-  // default settings
-  // (you can also pass in a Wire library object like &Wire2)
-  status = bme.begin(0x77);
-  if (!status)
-  {
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    while (1);
-  }
-
-  Serial.println();
 }
 
 void loop() {
-  Serial.println("----- loop() -----");
   //use the functions which are supplied by library.
   double const humidity = dht.readHumidity();
   // Read temperature as Celsius (the default)
   double const temperature = dht.readTemperature();
   // Check if any reads failed and exit early (to try again).
-  if (isnan(humidity) || isnan(temperature))
-  {
+  if (isnan(humidity) || isnan(temperature)) {
     Serial.println("Failed to read from DHT sensor!");
     delay(1000); // wait a bit
     return;
@@ -78,33 +49,9 @@ void loop() {
   // Convert raw temperature in F to Celsius degrees
   Serial.print("Temperature (internal): ");
   Serial.print((temprature_sens_read() - 32) / 1.8);
-  Serial.println(" °C");
-
-  printValues();
+  Serial.println(" C");
 
   // delay a bit for the next read
   delay(2000);
 }
 
-void printValues()
-{
-  Serial.println("----- printValues() -----");
-  Serial.print("Temperature = ");
-  Serial.print(bme.readTemperature());
-  Serial.println(" °C");
-
-  Serial.print("Pressure = ");
-
-  Serial.print(bme.readPressure() / 100.0F);
-  Serial.println(" hPa");
-
-  Serial.print("Approx. Altitude = ");
-  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
-  Serial.println(" m");
-
-  Serial.print("Humidity = ");
-  Serial.print(bme.readHumidity());
-  Serial.println(" %");
-
-  Serial.println();
-}
