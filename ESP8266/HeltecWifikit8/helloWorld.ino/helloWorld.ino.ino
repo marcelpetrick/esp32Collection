0629aa76440479ec46b3d76fa96d904897fea988
mail@marcelpetrick.it <mail@marcelpetrick.it>
2019-06-11 23:06:17 +0200

    wifikit8: refined the output :)
    Switching between "Laku noc," and "Puska" on two different rows in intervals of 1 second.

--- ESP8266/HeltecWifikit8/helloWorld.ino/helloWorld.ino.ino
+++ ESP8266/HeltecWifikit8/helloWorld.ino/helloWorld.ino.ino
@@ -4,7 +4,8 @@
 // Pin 16   OLED Reset
 // Pin 4,5  I2C
 
-// use Board "Wemos D1 Mini R2", 80Mhz, 4(3)MB Flash, 921600baud to flash
+// use Board "Wemos D1 Mini R2", 80Mhz, 4 MB Flash, 921600baud to flash
+// COM10
 
 U8X8_SSD1306_128X32_UNIVISION_HW_I2C u8x8(16);
 
@@ -16,7 +17,10 @@ void setup(void)
 void loop(void)
 {
   u8x8.setFont(u8x8_font_chroma48medium8_r);
-  u8x8.draw2x2String(0,0,"laku noc");
-  u8x8.draw2x2String(0,2,"Puska! <3");
+  u8x8.drawString(0, 0, "Laku noc,");
   delay(1000);
+  u8x8.clearLine(0);
+  u8x8.drawString(0, 2, "Puska! <3");
+  delay(1000);
+  u8x8.clearLine(2);
 }
